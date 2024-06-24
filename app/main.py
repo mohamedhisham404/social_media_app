25d3663f90dae70ed7ef96a979b083610c8014c3
mohamedhisham404 <mohamedhisham694@gmail.com>
2024-06-24 20:52:50 +0300

    use ORM

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI,status,HTTPException,Response
+from fastapi import FastAPI,status,HTTPException,Response,Depends
 from pydantic import BaseModel
 from typing import Optional
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from sqlalchemy.orm import Session
+from . import models
+from.database import engine,get_db
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -11,7 +16,7 @@ class post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating:Optional[int] = None
+    created_at: Optional[str] = None
 
 while True:
     try:
@@ -31,37 +36,30 @@ while True:
 
 @app.get("/")
 def read_root():
-    return {"hello": "World"}
+    return {"hello": "World"}   
 
 @app.get("/posts")
-def get_posts():
-    cur.execute("""SELECT * FROM posts""")
-    posts = cur.fetchall()
-    return {"data": posts}
+def get_posts(db: Session = Depends(get_db)):
+    posts=db.query(models.post).all()
+    return {"data":posts}
 
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
-def create_posts(new_post:post):
-    cur.execute("""INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING *""",
-                (new_post.title,new_post.content,new_post.published))
-    
-    post_dict=cur.fetchone()
-    conn.commit()
+def create_posts(new_post:post, db: Session = Depends(get_db)):
+    post_dict = models.post(**new_post.dict())
+    db.add(post_dict)
+    db.commit()
+    db.refresh(post_dict)
+
     return {"data":post_dict}
 
 @app.get("/posts/latest")
-def get_latest_post():
-    cur.execute("""SELECT * from posts ORDER BY created_at DESC LIMIT 1""")
-
-    post_dict=cur.fetchone()
-    conn.commit()
+def get_latest_post(db: Session = Depends(get_db)):
+    post_dict=db.query(models.post).order_by(models.post.created_at.desc()).first()
     return {"data":post_dict}
 
 @app.get("/posts/{id}")
-def get_post(id:int):
-    cur.execute("""SELECT * from posts where id=%s""",(str(id)))
-
-    post_dict=cur.fetchone()
-    conn.commit()
+def get_post(id:int, db: Session = Depends(get_db)):
+    post_dict = db.query(models.post).filter(models.post.id==id).first()
     if post_dict:
         return {"data":post_dict}
 
@@ -70,28 +68,27 @@ def get_post(id:int):
 
 
 @app.delete("/posts/{id}" , status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id:int):
-    cur.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
-    post_dict = cur.fetchone()
-    conn.commit()
-    
-    if not post_dict:
+def delete_post(id:int, db: Session = Depends(get_db)):
+    post_query = db.query(models.post).filter(models.post.id==id)
+    post_dict = post_query.first()
+
+    if  post_dict==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f"Sorry, post with id {id} not found")
+                        detail=f"Sorry, post with id {id} not found")
     
+    post_query.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-
 @app.put("/posts/{id}" ,status_code= status.HTTP_202_ACCEPTED)
-def update_post(id:int,new_post:post):
-   cur.execute("""UPDATE POSTS SET title=%s,content=%s,published=%s WHERE id=%s RETURNING *""",
-               (new_post.title,new_post.content,new_post.published,str(id)))
-   
-   post_dict = cur.fetchone()
-   conn.commit()
+def update_post(id:int, updated_post:post,db: Session = Depends(get_db)):
+    post_query = db.query(models.post).filter(models.post.id==id)
+    post = post_query.first()
 
-   if not post_dict:
-       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                           detail=f"Sorry, post with id {id} not found")
-   
-   return {"data":post_dict}
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Sorry, post with id {id} not found")
+
+    post_query.update(updated_post.dict(),synchronize_session=False)
+    db.commit()
+    return {"data",post_query.first()}
---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from .database import Base
+
+class post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, index=True,nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, server_default='True')
+    created_at = Column(TIMESTAMP(timezone=True),server_default=text("now()"))
