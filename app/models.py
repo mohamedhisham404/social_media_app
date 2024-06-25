8c381fe957efa293053226e46c3c4e68d365fffc
mohamedhisham404 <mohamedhisham694@gmail.com>
2024-06-25 20:32:14 +0300

    add user to the schema

--- app/models.py
+++ app/models.py
@@ -11,4 +11,11 @@ class post(Base):
     published = Column(Boolean, server_default='True')
     created_at = Column(TIMESTAMP(timezone=True),server_default=text("now()"))
 
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True,nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),server_default=text("now()"))
+
 
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
 from typing import Optional
 from datetime import datetime
 
@@ -14,5 +14,17 @@ class PostResponse(PostBase):
     id: int
     created_at: datetime
 
+    class Config:
+        orm_mode = True
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+    
     class Config:
         orm_mode = True
