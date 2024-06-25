7e8b7e06cb76e65dae526b120d969c3da171c74d
mohamedhisham404 <mohamedhisham694@gmail.com>
2024-06-25 20:53:04 +0300

    get user by id

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
-from fastapi import FastAPI,status,HTTPException,Response,Depends
+import time
 import psycopg2
+from fastapi import FastAPI,status,HTTPException,Response,Depends
 from psycopg2.extras import RealDictCursor
 from typing import List
-import time
 from sqlalchemy.orm import Session
-from . import models,schemas
+from . import models,schemas,utils
 from.database import engine,get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+#######################################database#######################################
 while True:
     try:
         conn = psycopg2.connect(
@@ -26,6 +27,8 @@ while True:
     except (Exception, psycopg2.Error) as error :
         print ("Error while connecting to PostgreSQL", error)
         time.sleep(1)
+
+#######################################posts#######################################
 
 @app.get("/")
 def read_root():
@@ -86,11 +89,27 @@ def update_post(id:int, updated_post:schemas.PostCreate,db: Session = Depends(ge
     db.commit()
     return post_query.first()
 
+#######################################users#######################################
+
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
 def create_user(user:schemas.UserCreate,  db: Session = Depends(get_db)):
+    #creat hashed password
+    hashed_password = utils.hash(user.password)
+    user.password =  hashed_password
+
+    #create user
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
     return new_user
+
+@app.get("/users/{id}" , response_model=schemas.UserResponse)
+def get_user(id:int, db: Session = Depends(get_db)):
+    user_dict = db.query(models.User).filter(models.User.id==id).first()
+    if user_dict:
+        return user_dict
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                        detail=f"sorry, user with id {id} not found")
