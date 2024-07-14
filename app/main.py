0bdfb900fa280d4e3d046dfcd2331d95e4e674b0
mohamedhisham404 <mohamedhisham694@gmail.com>
2024-07-14 13:08:29 +0300

    add requirements file

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from . import models
 from.database import engine
 from .routers import users,posts,auth,vote
 from .config import Settings
+from fastapi.middleware.cors import CORSMiddleware
 
 # models.Base.metadata.create_all(bind=engine)
 
@@ -23,6 +24,16 @@ app = FastAPI()
 #     except (Exception, psycopg2.Error) as error :
 #         print ("Error while connecting to PostgreSQL", error)
 #         time.sleep(1)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(posts.router)
